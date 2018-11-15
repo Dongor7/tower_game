377c79b15bd4de027228403f2084338fe0193a78
Dongor7 <dongor7@gmail.com>
2018-11-15 22:06:37 +0300

    feat: add config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
     module: {
         rules: [
+            {
+                test: /\.sass$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+           }
 
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Tower Game'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
